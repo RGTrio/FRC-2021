a833c4e27320c080949f45ab2e7f23469801b489
RGTrio <ririyennu@gmail.com>
2020-12-11 17:10:21 -0600

    Create InputMethod.java

---
+++ src/main/java/frc/input/InputMethod.java
@@ -0,0 +1,32 @@
+package frc.input;
+
+/**
+ * Operator controls for the robot
+ */
+public abstract class InputMethod {
+    public boolean indexerPower()
+    {
+        return false;
+    }
+    public double leftSidePower() 
+    {
+        return 0;
+    }
+    
+    public double rightSidePower() 
+    {
+        return 0;
+    }
+    public boolean climbPower()
+    { 
+        return false;
+    }
+    public boolean controllerPower() 
+    {
+        return false;
+    }
+    public boolean shooterPower() 
+    {
+        return false;
+    }
+}
