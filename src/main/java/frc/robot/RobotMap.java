0502bf18cac88ca2690f459380428733c10c0f44
RGTrio <ririyennu@gmail.com>
2020-12-11 17:10:28 -0600

    Create RobotMap.java

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,11 @@
+package frc.robot;
+
+public class RobotMap {
+    public static final int INDEXER = 4;
+    public static final int CLIMB = 2;
+    public static final int SHOOTER = 3;
+    public static final int INTAKE = 6;
+    public static final int CONTROLLER = 5;
+    public static final int LEFT_DRIVE = 0;
+    public static final int RIGHT_DRIVE = 1;
+}
